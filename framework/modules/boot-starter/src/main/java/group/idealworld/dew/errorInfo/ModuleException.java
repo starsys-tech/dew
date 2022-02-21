864c416bd2079c902443f6b093aba52caf8b165f
nipeixuan <34534268+nipeixuan@users.noreply.github.com>
2022-02-21 09:42:15 +0800

    ERROR CODE

---
+++ framework/modules/boot-starter/src/main/java/group/idealworld/dew/errorInfo/ModuleException.java
@@ -0,0 +1,56 @@
+package group.idealworld.dew.errorInfo;
+
+import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
+
+public class ModuleException extends AbstractException{
+
+    private ErrorInfo errorCode;
+    /**
+     * 本地错误描述信息,其优先级高于{@link ErrorInfo}中的errorDesc,即如果该值不为空,则以该值为准
+     */
+    @Setter
+    private String errorDesc;
+
+    public ModuleException(ClientErrorCodeMark errorCode) {
+        super();
+        this.errorCode = errorCode;
+    }
+
+    public ModuleException(ClientErrorCodeMark errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public ModuleException(ClientErrorCodeMark errorCode, Throwable cause) {
+        super(cause);
+        this.errorCode = errorCode;
+    }
+
+    public ModuleException(ClientErrorCodeMark errorCode, String message, Throwable cause) {
+        super(message, cause);
+        this.errorCode = errorCode;
+    }
+
+    @Override
+    public String getErrorCode() {
+        return errorCode.getErrorCode();
+    }
+
+    @Override
+    public String getErrorDesc() {
+        if (!StringUtils.isEmpty(errorDesc)) {
+            return errorDesc;
+        }
+        return errorCode.getErrorDesc();
+    }
+
+    @Override
+    public String getMessage() {
+        String message = super.getMessage();
+        if (StringUtils.isEmpty(message)) {
+            message = this.getErrorDesc();
+        }
+        return message;
+    }
+}
