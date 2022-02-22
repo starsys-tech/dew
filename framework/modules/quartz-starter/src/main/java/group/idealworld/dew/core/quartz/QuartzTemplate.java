e7dfd2791c1d150071c262e5ab8ea0de3b98b73c
nipeixuan <34534268+nipeixuan@users.noreply.github.com>
2022-02-22 18:28:05 +0800

    add quartz module

--- framework/modules/quartz-starter/src/main/java/group/idealworld/dew/core/quartz/QuartzTemplate.java
+++ framework/modules/quartz-starter/src/main/java/group/idealworld/dew/core/quartz/QuartzTemplate.java
@@ -17,16 +17,16 @@ public class QuartzTemplate implements QuartzOperation {
     }
 
     @Override
-    public void addJob(String clazzName, String jobName, String groupName, String cronExp, Map<String, Object> param) {
+    public void addJob(String clazzName, String jobkey, String cronExp, Map<String, Object> param) {
         try {
 //            scheduler.start();
             //构建job信息
             Class<? extends Job> jobClass = (Class<? extends Job>) Class.forName(clazzName);
-            JobDetail jobDetail = JobBuilder.newJob(jobClass).withIdentity(jobName, groupName).build();
+            JobDetail jobDetail = JobBuilder.newJob(jobClass).withIdentity(jobkey).build();
             //表达式调度构建器(即任务执行的时间)
             CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(cronExp);
             //按新的cronExpression表达式构建一个新的trigger
-            CronTrigger trigger = TriggerBuilder.newTrigger().withIdentity(jobName, groupName).withSchedule(scheduleBuilder).build();
+            CronTrigger trigger = TriggerBuilder.newTrigger().withIdentity(jobkey).withSchedule(scheduleBuilder).build();
             //获得JobDataMap，写入数据
             if (param != null) {
                 trigger.getJobDataMap().putAll(param);
@@ -38,36 +38,36 @@ public class QuartzTemplate implements QuartzOperation {
     }
 
     @Override
-    public void pauseJob(String jobName, String groupName) {
+    public void pauseJob(String jobKey) {
         try {
-            scheduler.pauseJob(JobKey.jobKey(jobName, groupName));
+            scheduler.pauseJob(JobKey.jobKey(jobKey));
         } catch (SchedulerException e) {
             log.error("暂停任务失败", e);
         }
     }
 
     @Override
-    public void resumeJob(String jobName, String groupName) {
+    public void resumeJob(String jobKey) {
         try {
-            scheduler.resumeJob(JobKey.jobKey(jobName, groupName));
+            scheduler.resumeJob(JobKey.jobKey(jobKey));
         } catch (SchedulerException e) {
             log.error("恢复任务失败", e);
         }
     }
 
     @Override
-    public void runOnce(String jobName, String groupName) {
+    public void runOnce(String jobKey) {
         try {
-            scheduler.triggerJob(JobKey.jobKey(jobName, groupName));
+            scheduler.triggerJob(JobKey.jobKey(jobKey));
         } catch (SchedulerException e) {
             log.error("立即运行一次定时任务失败", e);
         }
     }
 
     @Override
-    public void updateJob(String jobName, String groupName, String cronExp, Map<String, Object> param) {
+    public void updateJob(String jobKey, String cronExp, Map<String, Object> param) {
         try {
-            TriggerKey triggerKey = TriggerKey.triggerKey(jobName, groupName);
+            TriggerKey triggerKey = TriggerKey.triggerKey(jobKey);
             CronTrigger trigger = (CronTrigger) scheduler.getTrigger(triggerKey);
             if (cronExp != null) {
                 // 表达式调度构建器
@@ -87,12 +87,12 @@ public class QuartzTemplate implements QuartzOperation {
     }
 
     @Override
-    public void deleteJob(String jobName, String groupName) {
+    public void deleteJob(String jobKey) {
         try {
             //暂停、移除、删除
-            scheduler.pauseTrigger(TriggerKey.triggerKey(jobName, groupName));
-            scheduler.unscheduleJob(TriggerKey.triggerKey(jobName, groupName));
-            scheduler.deleteJob(JobKey.jobKey(jobName, groupName));
+            scheduler.pauseTrigger(TriggerKey.triggerKey(jobKey));
+            scheduler.unscheduleJob(TriggerKey.triggerKey(jobKey));
+            scheduler.deleteJob(JobKey.jobKey(jobKey));
         } catch (Exception e) {
             log.error("删除任务失败", e);
         }
@@ -136,4 +136,15 @@ public class QuartzTemplate implements QuartzOperation {
             log.error("关闭所有的任务失败", e);
         }
     }
+
+    @Override
+    public boolean checkJob(String jobKey) throws SchedulerException {
+        return scheduler.checkExists(new JobKey(jobKey));
+    }
+
+    public String getJobState(String jobKey) throws SchedulerException {
+        return scheduler.getTriggerState(TriggerKey.triggerKey(jobKey)).toString();
+    }
+
+
 }
